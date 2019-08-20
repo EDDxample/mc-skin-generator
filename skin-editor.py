477126a8b287c9923e112da1dcbe7f1ccc52c716
EDDxample <EDDxample@gmail.com>
2019-08-20 19:09:53 +0200

    fix skin editor to work with gan

--- skin-editor.py
+++ skin-editor.py
@@ -1,20 +1,22 @@
 import pygame, sys, PIL, numpy as np, random as rng
 from pygame.locals import *
+
+import os ; os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
+
 from keras.models import load_model
 
-GENERATOR = load_model('mc-generator.h5')
-NOISE = np.random.uniform(size=(1, 1, GENERATOR.get_input_shape_at(0)[2])) * 4000 - 2000
+GENERATOR = load_model('output/generator_epoch10.h5')
+NOISE = np.random.normal(0, 1, [1, GENERATOR.get_input_shape_at(0)[-1]])
 
-def map(value, start, stop):
-    _MIN, _MAX = -2000, 2000
+def map(value, _MIN, _MAX, start, stop):
     return start + (stop - start) * ((value - _MIN) / (_MAX - _MIN))
 
 def gen_pic(windowSurface):
     global GENERATOR, NOISE
     arr = GENERATOR.predict(NOISE)
-    arr = arr.reshape(int(arr.shape[2]/256), 64, 4) * 255
+    arr = arr.reshape(int(arr.shape[-1]/256), 64, 4) * 255
     pic = PIL.Image.fromarray(arr.astype('uint8'))
-    # pic.save('pic.png')
+    pic.save('output/pic.png')
     surface = pygame.image.fromstring(pic.tobytes(), pic.size, 'RGBA')
     surface = pygame.transform.scale(surface, (500, 500))
     windowSurface.blit(surface, (0, 0))
@@ -32,10 +34,12 @@ dx = 300 // 10
 dy = 500 // 10
 def draw_cells():
     global NOISE
+    MIN, MAX = np.amin(NOISE), np.amax(NOISE)
+    print(MIN, MAX)
     index = 0
     for y in range(0, 500, dy):
         for x in range(500, 800, dx):
-            col = map(NOISE[0,0,index], 0, 255)
+            col = map(NOISE[0,index], MIN, MAX, 0, 255)
             index += 1
             pygame.draw.rect(windowSurface, (col, col, col),Rect(x, y, dx -1, dy -1))
 
@@ -51,8 +55,8 @@ while True:
             cellpos = ((pos[0] - 500)//dx, pos[1]//dy)
             if cellpos[0] >= 0 and cellpos[1] >= 0:
                 index = cellpos[0] + cellpos[1] * 10
-                NOISE[0,0,index] = rng.randint(-2000, 2000)
-                windowSurface.fill((128, 128, 150))
+                NOISE[0,index] = rng.randint(-3, 3)
+                windowSurface.fill((0, 0, 0))
                 gen_pic(windowSurface)
                 draw_cells()
                 pygame.display.update()
