5ddd47e1e1ee6a41b9e4ca4d1b8bbcdcb430380d
EDDxample <EDDxample@gmail.com>
2019-08-20 19:09:46 +0200

    create and train gan

--- gan-attempt/gan.py
+++ gan-attempt/gan.py
@@ -1,12 +1,44 @@
-import numpy as np
+import numpy as np, matplotlib.pyplot as plt
+
+# Change keras backend
+import os ; os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
+
 from keras import Sequential, Model, Input
 from keras.layers import Dense, LeakyReLU, Dropout
 
-def main():
+def main(epochs=1, batch_size=128):
+    skin_dataset = np.load('models/mc-skins-64x64.npy') # (5578, 16384)
+    
     generator = create_generator()
     discriminator = create_discriminator()
     gan = create_gan(discriminator, generator)
-    gan.summary()
+    
+    for e in range(epochs):
+        print(f'Epoch {e}')
+        for _ in range(batch_size):
+            noise = np.random.normal(0,1, [batch_size, 100])
+            
+            fake_skins = generator.predict(noise)
+            real_skins = skin_dataset[np.random.randint(low=0, high=skin_dataset.shape[0], size=batch_size)]
+            X = np.concatenate([real_skins, fake_skins])
+
+            Y = np.zeros(2*batch_size)
+            Y[:batch_size] = 0.9
+            
+            discriminator.trainable = True
+            discriminator.train_on_batch(X, Y)
+            discriminator.trainable = False
+
+            X = np.random.normal(0,1, [batch_size, 100])
+            Y = np.ones(batch_size)
+
+            gan.train_on_batch(X, Y)
+
+        if e % 10 == 0:
+            plot_imgs(e, generator)
+            generator.save(f'output/generator_epoch{e}.h5')
+    generator.save(f'output/generator_final.h5')
+
 
 
 def create_generator():
@@ -18,7 +50,6 @@ def create_generator():
     g.add(Dense(64*64*4,              activation='sigmoid')) # Tut: tanh
     g.compile(optimizer='adam', loss='mse') # Tut: binary_crossentropy
     return g
-
 def create_discriminator():
     d = Sequential()
     d.add(Dense(2048, input_dim=64*64*4)) ; d.add(LeakyReLU(0.2)) ; d.add(Dropout(0.3))
@@ -28,8 +59,9 @@ def create_discriminator():
     d.add(Dense(1,                         activation='sigmoid'))
     d.compile(optimizer='adam', loss='binary_crossentropy')
     return d
-
 def create_gan(discriminator, generator):
+    discriminator.trainable = False
+
     gan_input = Input((100,))
     x = generator(gan_input)
     gan_output = discriminator(x)
@@ -37,4 +69,16 @@ def create_gan(discriminator, generator):
     gan.compile(optimizer='adam', loss='binary_crossentropy')
     return gan
 
-if __name__ == "__main__": main()
+def plot_imgs(epoch, generator, examples=100, dim=(10,10), figsize=(10,10)):
+    noise= np.random.normal(loc=0, scale=1, size=[examples, 100])
+    generated_images = generator.predict(noise)
+    generated_images = generated_images.reshape(examples,64,64,4)
+    plt.figure(figsize=figsize)
+    for i in range(generated_images.shape[0]):
+        plt.subplot(dim[0], dim[1], i+1)
+        plt.imshow(generated_images[i])
+        plt.axis('off')
+    plt.tight_layout()
+    plt.savefig(f'output/gan_generated_image_{epoch}.png')
+
+if __name__ == "__main__": main(20)
